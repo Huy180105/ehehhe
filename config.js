6fd57b60d1cf4c170e67497a2c2746debd7ce7cb
Huy180105 <huy74dcht21108@st.utt.edu.vn>
2024-11-14 00:48:01 +0700

    crush

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Miss",
+    introTitle: 'Tên ny bạn',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: '...',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://i.pinimg.com/originals/82/06/af/8206af9d1a00baeb8644c72f2e3bd089.png'
+}
