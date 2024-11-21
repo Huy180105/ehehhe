26ab69f335128e2253321cf6522b9c5bc1b6c8a7
Huy180105 <huy74dcht21108@st.utt.edu.vn>
2024-11-21 21:54:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Miss",
-    introTitle: 'Vy Lâm',
+    introTitle: 'Quốc Lĩnh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
